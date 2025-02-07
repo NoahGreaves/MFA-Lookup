4ca861c49f34367f7d00396632e2e5fcfd6744a6
Noah Greavecs <noahgreaves@gmail.com>
2025-02-06 22:13:17 -0700

    cleaned code

--- app/page.js
+++ app/page.js
@@ -14,64 +14,23 @@ import AuthenticationButton from './Componenets/AuthenticationButton';
 // STARTS LOCAL SERVER TO TEST REACT APP
 
 import "./css/page.css";
-import oktaConfig from "./config/oktaConfig.js";
-import { BrowserRouter } from "react-router-dom";
-import { Security, useOktaAuth } from "@okta/okta-react";
-import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
-import { Route, Switch, useHistory } from "react-router-dom";
 
 export default function HomePage() {
 
-  // const oktaAuth = new OktaAuth(oktaConfig);
-
-  // const history = useHistory();
-  //       const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-  //         history.replace(toRelativeUrl(originalUri || "/", window.location.origin));
-  //     };
-  // useEffect(() => {
-  //   const allWithClass = Array.from(
-  //     document.getElementsByClassName('auth-button')
-  //   );
-  //   console.log(allWithClass);
-  // }, []);
-
-  // const login = () => {
-  //   console.log("cliked")
-  //   navigate("/api/auth/login"); {/* navigate to desired page */ }
-  // };
-
-  // const logout = () => {
-  //   navigate("/api/auth/logout");
-  // };
-
   return (
-    // <Security oktaAuth={this.oktaConfig}>
+    <div>
+      <div className="containerColumn">
+        <Header title="ATB Multi-Factor Authentication (MFA) Account Search" />
 
-      <BrowserRouter>
-        <div>
-          <div className="containerColumn">
-            <Header title="ATB Multi-Factor Authentication (MFA) Account Search" />
-            {/* Login / Logout Buttons */}
-            <div className="containerRow">
-              {/* <button className="auth-button" onClick={login}></button> */}
+        {/* Login and Logout buttons */}
+        <div className="containerRow">
+          <a className="button" href="/api/auth/login">Login</a>
+          <a className="button" href="/api/auth/logout">Logout</a>
 
-              {/* <AuthenticationButton></AuthenticationButton> */}
-
-              {/* <div className="auth-button" style={buttonStyle}> */}
-              <a href="/api/auth/login">Login</a>
-              {/* </div> */}
-
-              {/* <div className="auth-button" style={buttonStyle}> */}
-              <a href="/api/auth/logout">Logout</a>
-              {/* </div> */}
-            </div>
-            <ProfileClient></ProfileClient>
-            {/* <ProfileServer></ProfileServer> */}
-            <SearchBar className="inputFieldStyle" />
-          </div>
         </div>
-      </BrowserRouter>
-    // </Security>
-
+        <ProfileClient></ProfileClient>
+        <SearchBar className="inputFieldStyle" />
+      </div>
+    </div>
   );
 }
